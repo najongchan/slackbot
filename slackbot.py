29cb613c9c7b4881defa89b70658f9fd6c14ef4c
나종찬 <najongchan@najongchan-ui-MacBook-Pro.local>
2018-04-14 17:51:47 +0900

    launch recommand slack bot

--- slackbot.py
+++ slackbot.py
@@ -1,14 +1,25 @@
 from slacker import Slacker
 import pytz
+import random
+import time
 import github3
 import datetime
 
 localTimeZone = pytz.timezone('Asia/Seoul')
-token = 'xoxb-347891948695-mU7krv7r2hIMJsz5u5aAMhyn'
+token = 'xoxb-347891948695-s3fUmvgxmBobgUeCIrckRBEi'
 slack = Slacker(token)
+username = '점심추천봇'
+icon_emoji = ':dart:'
 channel = ['#bottest', '#general', '#nago']
+lunchlist = [u'굶기', u'다이어트']
 
-def poatMessage(channel, message) :
-    slack.chat.post_message(channel, message, as_user=True)
+attachement = {
+    "pretext":"오늘의 점심은...?",
+    "fallback":"점심추천봇",
+    "text": lunchlist[random.randint(0, len(lunchlist) - 1)],
+}
 
-print("second try")
+def poatMessage(channel, attachement, icon_emoji) :
+    slack.chat.post_message(channel=channel, text=None, attachments=[attachement], icon_emoji=icon_emoji, as_user=False)
+
+poatMessage(channel[0], attachement, icon_emoji)
